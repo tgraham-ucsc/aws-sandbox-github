f1208627d6231cc9aa1e7a877e9048d8df1639bf
Cloud User <ec2-user@ip-172-31-24-238.us-west-2.compute.internal>
2016-10-14 03:30:35 -0400

    adding some more users

--- myfirstcookbook/recipes/users.rb
+++ myfirstcookbook/recipes/users.rb
@@ -3,3 +3,32 @@ user "tgraham" do
   home "/home/tgraham"
   shell "/bin/bash"
 end
+
+user "penniman" do
+  comment "default user"
+  home "/home/penniman"
+  shell "/bin/bash"
+end
+
+user "ama149" do
+  comment "default user"
+  home "/home/ama149"
+  shell "/bin/bash"
+end
+
+user "jkeh" do
+  comment "default user"
+  home "/home/jkeh"
+  shell "/bin/bash"
+end
+
+user "banner" do
+  comment "default user"
+  home "/opt/app/banner"
+  shell "/bin/bash"
+end
+
+user "fisjob" do
+  comment "default user"
+  home "/opt/app/fisjob"
+  shell "/bin/bash"
