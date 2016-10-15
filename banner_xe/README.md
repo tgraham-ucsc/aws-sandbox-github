ce9a8f88cb155d97112541455924ff179e36ef42
Todd Graham <ec2-user@ip-172-31-24-238.us-west-2.compute.internal>
2016-10-15 13:47:10 -0400

    adding banner_xe cookbook

---
+++ banner_xe/README.md
@@ -0,0 +1,4 @@
+# banner_xe
+
+TODO: Enter the cookbook description here.
+
---
+++ banner_xe/metadata.rb
@@ -0,0 +1,15 @@
+name 'banner_xe'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures banner_xe'
+long_description 'Installs/Configures banner_xe'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/banner_xe/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/banner_xe' if respond_to?(:source_url)
