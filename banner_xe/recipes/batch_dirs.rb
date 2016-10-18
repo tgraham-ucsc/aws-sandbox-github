f3f4f40705580f2122af3995f8b6db303f58a2ae
Todd Graham <tgraham@ucsc.edu>
2016-10-18 07:29:28 -0700

    reverting back to the single directory for now

--- banner_xe/recipes/batch_dirs.rb
+++ banner_xe/recipes/batch_dirs.rb
@@ -4,24 +4,3 @@ directory "/opt/app/" do
   group 'root'
   action :create
 end
-
-directory "/opt/app/banner" do
-  mode 0755
-  owner 'banner'
-  group 'banner'
-  action :create
-end
-
-directory "/opt/app/oracle" do
-  mode 0755
-  owner 'oracle'
-  group 'oracle'
-  action :create
-end
-
-directory "/opt/app/media" do
-  mode 0777
-  owner 'banner'
-  group 'banner''
-  action :create
-end
