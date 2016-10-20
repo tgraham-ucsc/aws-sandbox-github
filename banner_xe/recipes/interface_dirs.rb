e2eadf33160829d500da703d89536f1f4cb87ea7
Todd Graham <tgraham@ucsc.edu>
2016-10-20 16:56:41 -0700

    deleted a redundant entry for aisfeed

--- banner_xe/recipes/interface_dirs.rb
+++ banner_xe/recipes/interface_dirs.rb
@@ -18,3 +18,46 @@ directory "/opt/app/interfaces" do
   group 'root'
   action :create
 end
+
+directory "/opt/app/interfaces/fisxfer" do
+  mode 0755
+  owner 'fisxfer'
+  group 'fisxfer'
+  action :create
+end
+
+directory "/opt/app/interfaces/fisxfer/prntsrvs" do
+  mode 0770
+  owner 'fisxfer'
+  group 'prntsrvs'
+  action :create
+end
+
+
+directory "/opt/app/interfaces/fisxfer/aisfeed" do
+  mode 0770
+  owner 'fisxfer'
+  group 'aisfeed'
+  action :create
+end
+
+directory "/opt/app/interfaces/fisxfer/bslfmw" do
+  mode 0770
+  owner 'fisxfer'
+  group 'bslfmw'
+  action :create
+end
+
+directory "/opt/app/interfaces/fisxfer/mailsrvs" do
+  mode 0770
+  owner 'fisxfer'
+  group 'mailsrvs'
+  action :create
+end
+
+directory "/opt/app/interfaces/fisxfer/surplus" do
+  mode 0770
+  owner 'fisxfer'
+  group 'surplus'
+  action :create
+end
