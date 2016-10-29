ce4bf9c2998840dc4768739afbc32ca79641623f
Todd Graham <tgraham@ucsc.edu>
2016-10-29 09:50:47 -0700

    refined syntax

--- banner_xe/recipes/batch_packages.rb
+++ banner_xe/recipes/batch_packages.rb
@@ -1,15 +1,4 @@
-package 'git' do
-    action:install
-end
-
-package 'tree' do
-    action:install
-end
-
-package 'vim' do
-    action:install
-end
-
-package 'ksh' do
-    action:install
-end
+package 'git'
+package 'tree'
+package 'vim' 
+package 'ksh'
