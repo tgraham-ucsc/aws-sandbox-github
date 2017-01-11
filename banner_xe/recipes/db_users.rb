a4abce971a5b09da65e49f81f8894ba7be84821b
Iran Rodrigues <imrodrig@ucsc.edu>
2017-01-11 15:40:12 -0800

    tst-imrodrig

--- banner_xe/recipes/db_users.rb
+++ banner_xe/recipes/db_users.rb
@@ -1,39 +1,3 @@
-user "tgraham" do
-  comment "default user"
-  home "/home/tgraham"
-  shell "/bin/bash"
-end
-
-user "penniman" do
-  comment "default user"
-  home "/home/penniman"
-  shell "/bin/bash"
-end
-
-user "ama149" do
-  comment "default user"
-  home "/home/ama149"
-  shell "/bin/bash"
-end
-
-user "jkeh" do
-  comment "default user"
-  home "/home/jkeh"
-  shell "/bin/bash"
-end
-
-user "banner" do
-  comment "default user"
-  home "/home/banner"
-  shell "/bin/bash"
-end
-
-user "fisjob" do
-  comment "default user"
-  home "/home/fisjob"
-  shell "/bin/bash"
-end
-
 user "oracle" do
   comment "default user"
   home "/home/oracle"
