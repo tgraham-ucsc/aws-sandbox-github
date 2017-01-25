a38c309e54e3c4dc09e7c15c5475956d72f0a762
Todd Graham <tgraham@ucsc.edu>
2017-01-25 14:25:32 -0800

    adding oracle client cookbook from chef market place

--- banner_xe/README.md
+++ banner_xe/README.md
@@ -1,4 +1,5 @@
 # banner_xe
 
 TODO: Enter the cookbook description here.
+Other Stuff
 
--- banner_xe/recipes/interface_build.rb
+++ banner_xe/recipes/interface_build.rb
@@ -1,3 +1,5 @@
+# Setup up the interface directory strucuture
+
 directory "/opt/app/"
 directory "/opt/app/interfaces"
 directory "/opt/app/interfaces"
@@ -7,6 +9,8 @@ directory "/opt/app/interfaces/fisxfer/aisfeed"
 directory "/opt/app/interfaces/fisxfer/bslfmw"
 directory "/opt/app/interfaces/fisxfer/mailsrvs"
 directory "/opt/app/interfaces/fisxfer/surplus"
+
+#Create the interface users and owners
 
 user "fisxfer" do
   comment "Parent user to consume interface files"
@@ -44,69 +48,58 @@ user "surplus" do
   shell "/bin/bash"
 end
 
-
-
+# Set the permissions and ownership on the interfact directories
 
 directory "/opt/app/" do
   mode 0755
   owner 'root'
   group 'root'
-  action :create
 end
 
 directory "/opt/app/interfaces" do
   mode 0755
   owner 'root'
   group 'root'
-  action :create
 end
 
 directory "/opt/app/interfaces" do
   mode 0755
   owner 'root'
   group 'root'
-  action :create
 end
 
 directory "/opt/app/interfaces/fisxfer" do
   mode 0755
   owner 'fisxfer'
   group 'fisxfer'
-  action :create
 end
 
 directory "/opt/app/interfaces/fisxfer/prntsrvs" do
   mode 0770
   owner 'fisxfer'
   group 'prntsrvs'
-  action :create
 end
-
 
 directory "/opt/app/interfaces/fisxfer/aisfeed" do
   mode 0770
   owner 'fisxfer'
   group 'aisfeed'
-  action :create
 end
 
 directory "/opt/app/interfaces/fisxfer/bslfmw" do
   mode 0770
   owner 'fisxfer'
   group 'bslfmw'
-  action :create
 end
 
 directory "/opt/app/interfaces/fisxfer/mailsrvs" do
   mode 0770
   owner 'fisxfer'
   group 'mailsrvs'
-  action :create
 end
 
 directory "/opt/app/interfaces/fisxfer/surplus" do
   mode 0770
   owner 'fisxfer'
   group 'surplus'
-  action :create
 end
